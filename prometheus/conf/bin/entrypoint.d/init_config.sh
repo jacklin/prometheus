f5f8febb3e17f66a8cd6835c9f95d4a03a0663e0
超级手艺人 <fjxmljc007@163.com>
2021-07-07 17:03:04 +0800

    添加支持多个job

--- prometheus/conf/bin/entrypoint.d/init_config.sh
+++ prometheus/conf/bin/entrypoint.d/init_config.sh
@@ -10,3 +10,23 @@ sed -ie "s#<TARGETS>#$TARGETS#" /etc/prometheus/prometheus.yml
 if [[ ! -d /prometheus/$HOSTNAME ]]; then
 	mkdir -p /prometheus/$HOSTNAME
 fi
+
+if [[ "$JOB_NAMES" != "" && "$JOB_TARGETS" != "" ]]; then
+	n=0
+	for jt in $JOB_TARGETS; do
+		n=`expr $n + 1`
+		eval jts$n="$jt"
+	done
+	# eval echo "The $n element of array is: \$jts1"
+	# eval echo "The $n element of array is: \$jts2"
+	m=0
+	echo "" >> /etc/prometheus/prometheus.yml
+	for jn in ${JOB_NAMES}; do
+		m=`expr $m + 1`
+		echo "  - job_name: ${jn}"  >> /etc/prometheus/prometheus.yml
+		echo "    static_configs:"   >> /etc/prometheus/prometheus.yml
+		eval echo "\  \  - targets: \$jts$m" >> /etc/prometheus/prometheus.yml
+		# echo $l >> /etc/prometheus/prometheus.yml
+	done
+	cat /etc/prometheus/prometheus.yml
+fi
